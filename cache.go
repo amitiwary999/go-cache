0cf3534758402096cfd077756a52ae71c9f939f9
amitiwary999 <tiwarya9@gmail.com>
2024-06-24 18:01:46 +0530

    cleanup cache

--- cache.go
+++ cache.go
@@ -1,26 +1,53 @@
 package inmemeorycache
 
+import "time"
+
 type CacheItem[T any] struct {
-	Item       T
-	Expiration uint32
+	item       T
+	expiration int64
 }
 
 type InMemoryCache[T any] struct {
-	Data            map[string]CacheItem[T]
-	CleanupInterval uint16
+	data            map[string]CacheItem[T]
+	cleanupInterval uint16
+	done            chan int
 }
 
-func NewInMemoryCache[T any](initialCapacity, cleanupInterval uint16) *InMemoryCache[T] {
-	return &InMemoryCache[T]{
-		Data:            make(map[string]CacheItem[T], initialCapacity),
-		CleanupInterval: cleanupInterval,
+func NewInMemoryCache[T any](initialCapacity, cleanupInterval uint16, done chan int) *InMemoryCache[T] {
+	cache := &InMemoryCache[T]{
+		data:            make(map[string]CacheItem[T], initialCapacity),
+		cleanupInterval: cleanupInterval,
+		done:            done,
+	}
+	cache.cleanUp()
+	return cache
+}
+
+func (c *InMemoryCache[T]) deleteExpiredItem() {
+	currentSecond := time.Now().Unix()
+	for k, v := range c.data {
+		if v.expiration >= currentSecond {
+			delete(c.data, k)
+		}
 	}
 }
 
-func (c *InMemoryCache[T]) Set(key string, value T, expirationSecond uint32) {
+func (c *InMemoryCache[T]) cleanUp() {
+	timer := time.NewTicker(time.Duration(c.cleanupInterval) * time.Second)
+	for {
+		select {
+		case <-timer.C:
+			c.deleteExpiredItem()
+		case <-c.done:
+			return
+		}
+	}
+}
+
+func (c *InMemoryCache[T]) Set(key string, value T, expirationSecond int64) {
 	item := CacheItem[T]{
-		Item:       value,
-		Expiration: expirationSecond,
+		item:       value,
+		expiration: expirationSecond,
 	}
-	c.Data[key] = item
+	c.data[key] = item
 }
