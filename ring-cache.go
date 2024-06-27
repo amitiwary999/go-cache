85ada2698aaf96222f4251d7d5398702629465f0
amitiwary999 <tiwarya9@gmail.com>
2024-06-27 21:06:30 +0530

    save the data in cache ring

--- ring-cache.go
+++ ring-cache.go
@@ -4,19 +4,50 @@ import (
 	"container/ring"
 )
 
-type cacheRing struct {
+type cacheRing[T any] struct {
 	data map[string]*ring.Ring
 	hand *ring.Ring
 }
 
-type cacheItem[T any] struct {
-	item T
+type cacheRingItem[T any] struct {
+	key       string
+	item      T
+	reference int8
 }
 
-func NewCacheRing(capacity int32) *cacheRing {
+func NewCacheRing[T any](capacity int32) *cacheRing[T] {
 	r := ring.New(int(capacity))
-	return &cacheRing{
+	return &cacheRing[T]{
 		data: make(map[string]*ring.Ring),
 		hand: r,
 	}
+}
+
+func (c *cacheRing[T]) findReplaceItem() {
+	for c.hand.Value.(*cacheRingItem[T]).reference == 1 {
+		c.hand.Value.(*cacheRingItem[T]).reference = 0
+		c.hand = c.hand.Next()
+	}
+	if c.hand.Value != nil {
+		currentItem := c.hand.Value.(*cacheRingItem[T])
+		delete(c.data, currentItem.key)
+		c.hand.Value = nil
+	}
+}
+
+func (c *cacheRing[T]) Set(key string, value T) {
+	item := &cacheRingItem[T]{
+		key:       key,
+		item:      value,
+		reference: 0,
+	}
+	if c.hand.Value == nil {
+		c.hand.Value = item
+		c.data[key] = c.hand
+	} else {
+		c.findReplaceItem()
+		c.hand.Value = item
+		c.data[key] = c.hand
+	}
+	c.hand = c.hand.Next()
 }
