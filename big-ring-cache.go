8995a16342ef6c49414314d9365f7397db3be5df
amitiwary999 <tiwarya9@gmail.com>
2024-06-29 18:22:21 +0530

    save data in file

--- big-ring-cache.go
+++ big-ring-cache.go
@@ -1,23 +1,42 @@
 package cache
 
-import "os"
+import (
+	"fmt"
+	"io"
+	"os"
+)
 
-type bigCacheRing[T any] struct {
+type bigCacheRing struct {
 	file      *os.File
 	offsetMap map[string]int64
-	cacheRing *cacheRing[T]
+	cacheRing *cacheRing[string]
 }
 
-func NewBigCacheRing[T any]() (*bigCacheRing[T], error) {
+func NewBigCacheRing() (*bigCacheRing, error) {
 	file, err := os.OpenFile("big-cache-ring-data.txt", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0644)
 	if err != nil {
 		return nil, err
 	}
-	cacheRing := NewCacheRing[T](1000)
+	cacheRing := NewCacheRing[string](1000)
 	offsetMap := make(map[string]int64)
-	return &bigCacheRing[T]{
+	return &bigCacheRing{
 		file:      file,
 		offsetMap: offsetMap,
 		cacheRing: cacheRing,
 	}, nil
+}
+
+func (c *bigCacheRing) Save(key string, value string) error {
+	offset, err := c.file.Seek(0, io.SeekEnd)
+	if err != nil {
+		fmt.Printf("big cache error seeking the file")
+		return err
+	}
+	fileData := key + " " + value
+	_, saveErr := c.file.WriteString(fileData)
+	if saveErr != nil {
+		return saveErr
+	}
+	c.offsetMap[key] = offset
+	return nil
 }
