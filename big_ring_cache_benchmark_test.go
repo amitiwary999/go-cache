f6f1e4edd86b2cbb743d87a229c6c949f928799d
amitiwary999 <tiwarya9@gmail.com>
2024-07-07 13:03:32 +0530

    benchmark

--- big_ring_cache_benchmark_test.go
+++ big_ring_cache_benchmark_test.go
@@ -8,44 +8,39 @@ import (
 var keyPref string = "key"
 var valuePref string = "value"
 
-func saveData(bgr *bigCacheRing) {
-	for i := 0; i < 100000; i++ {
+func saveData(bgr *bigCacheRing, start, end int) {
+	for i := start; i < end; i++ {
 		key := fmt.Sprintf("%v-%v", keyPref, i)
 		value := fmt.Sprintf("%v-%v", valuePref, i)
 		bgr.Save(key, value)
 	}
 }
 
-func Get(bgr *bigCacheRing) {
-	for i := 0; i < 25000; i++ {
+func Get(bgr *bigCacheRing, start, end int) {
+	for i := start; i < end; i++ {
 		key := fmt.Sprintf("%v-%v", keyPref, i)
 		bgr.Get(key)
 	}
 }
 
-func Delete(bgr *bigCacheRing) {
-	for i := 0; i < 10000; i++ {
+func Delete(bgr *bigCacheRing, start, end int) {
+	for i := start; i < end; i++ {
 		key := fmt.Sprintf("%v-%v", keyPref, i)
 		bgr.Delete(key)
 	}
 }
 
 func BenchmarkBigCache(b *testing.B) {
-	bgc, err := NewBigCacheRing(10000)
+	bgc, err := NewBigCacheRing(100000)
 	if err == nil {
-		for i := 0; i < 100000; i++ {
-			key := fmt.Sprintf("%v-%v", keyPref, i)
-			value := fmt.Sprintf("%v-%v", valuePref, i)
-			bgc.Save(key, value)
-		}
-
-		for i := 0; i < 25000; i++ {
-			key := fmt.Sprintf("%v-%v", keyPref, i)
-			bgc.Get(key)
-		}
-
-		saveData(bgc)
-		Delete(bgc)
-		Get(bgc)
+		saveData(bgc, 0, 500000)
+		Get(bgc, 0, 12000)
+		Delete(bgc, 0, 2000)
+		Delete(bgc, 13000, 15000)
+		Get(bgc, 12000, 25000)
+		saveData(bgc, 500000, 800000)
+		Get(bgc, 50000, 75000)
+		Delete(bgc, 100000, 120000)
+		Get(bgc, 110000, 130000)
 	}
 }
